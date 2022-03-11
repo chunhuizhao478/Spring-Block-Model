71d27691b23233957b2c4ee493aa658c5af63f41
chunhuizhao478 <93891863+chunhuizhao478@users.noreply.github.com>
2022-03-11 01:16:59 -0600

    Delete test_analytical.m

--- test_analytical.m
+++
@@ -1,7 +0,0 @@
-function [p_spatialtotal] = test_analytical(D,Q,k,mu,t,x)
-
-p_real = analytical_sol(D,Q,k,mu,t,x);
-%Flip the data
-p_flip = flip(p_real);
-%Assemble together
-p_spatialtotal = horzcat(p_flip(1:end-1),p_real);
